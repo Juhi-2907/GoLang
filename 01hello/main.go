78f65c12ec63bbad6485dc3cf73cdfb7a93b78fb
JYOTI SHARMA <42304578+Juhi-2907@users.noreply.github.com>
2024-01-05 18:12:47 +0530

    Update main.go

--- 01hello/main.go
+++ 01hello/main.go
@@ -4,5 +4,5 @@ import "fmt" //automatically get imported when we save the file because we insta
 
 func main() {
 	//any packeage public function name is started by capital letter.
-	fmt.Println("Hello Juhi!!!!!") //semicolon is not neccessary lexer will do for that. If we put lexer will remove that.
+	fmt.Println("Hello Juhi!!!!! How are you?") //semicolon is not neccessary lexer will do for that. If we put lexer will remove that.
 }
